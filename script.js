92afc6ba6670891e0794aef84d5bbd0670c84d53
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-01-16 15:36:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstWord(s) {
   // your code here
+	const words = str.trim().split(/\s+/); // Split by whitespace and remove extra spaces
+  return words.length > 0 ? words[0] : '';
+
 }
 
 // Do not change the code below
